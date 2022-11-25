00d73f4725330a5fe084f1b488de22b968d11fa0
Banerlc <118231956+Banerlc@users.noreply.github.com>
2022-11-25 15:34:29 -0500

    Update meme_flask.py

--- meme_flask.py
+++ meme_flask.py
@@ -17,3 +17,5 @@ def get_meme():
 @app.route("/")
 def index():
     meme_pic,subreddit = get_meme() return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
+
+app.run(host="0.0.0.0" , port=80)
